00f60a4d7ea935adfe34a422b8de6e2d9b04eb94
royqh1979 <royqh1979@gmail.com>
2019-12-24 15:37:00 +0800

    * add new example

---
+++ examples/beautiful.images/randomized.point.line.py
@@ -0,0 +1,29 @@
+"""
+
+Adapted from "Processing Creative Coding and Computational Art",Chapter 6, Page 181
+"""
+
+from easygraphics import *
+import random
+
+
+def main():
+    init_graph(600, 300)
+    width = get_width()
+    height = get_height()
+    set_background_color('black')
+    set_color('white')
+
+    random.seed()
+
+    rand = 0
+
+    for i in range(width):
+        draw_point(i, height / 2 + random.uniform(-rand, rand))
+        rand += random.uniform(-5, 5)
+
+    pause()
+    close_graph()
+
+
+easy_run(main)
